e92b9675485dd3cc96b3c8b4db6275603daf5189
aakansha79 <aakanshapandey78@gmail.com>
2026-03-08 21:50:13 +0530

    fix socket url

--- realtime-editor/src/pages/socket.js
+++ realtime-editor/src/pages/socket.js
@@ -1,20 +1,30 @@
 import { io } from "socket.io-client";
 
-let socket;
+let socket = null;
 
 export const initSocket = async () => {
+
+  const SOCKET_URL = window.location.origin;
+
   const options = {
     forceNew: true,
-    reconnectionAttempts: "Infinity",
+    reconnectionAttempts: Infinity,
     timeout: 10000,
     transports: ["websocket"],
   };
 
-  
-  socket = io(window.location.origin, options);
+  socket = io(SOCKET_URL, options);
+
+  socket.on("connect", () => {
+    console.log("Socket connected:", socket.id);
+  });
 
   socket.on("connect_error", (err) => {
     console.log("Socket connection failed:", err.message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected");
   });
 
   return socket;
